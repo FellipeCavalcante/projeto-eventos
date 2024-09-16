e8d8a1389dbeec8acc8993892df408e499cf73ce
FellipeCavalcante <azevedoaranha1@gmail.com>
2024-09-16 16:17:08 -0300

    creating entities

---
+++ api/src/main/java/com/eventotec/api/domain/coupon/Coupon.java
@@ -0,0 +1,32 @@
+package com.eventotec.api.domain.coupon;
+
+import com.eventotec.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "coupon")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coupon {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String code;
+    private Integer valid;
+    private Date date;
+
+    @ManyToOne
+    @JoinColumn(name = "event-id")
+    private Event event;
+}
