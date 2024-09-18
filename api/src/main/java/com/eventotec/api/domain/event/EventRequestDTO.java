b17741fc07b061a3ee299d8e33f07b72bf38fc57
FellipeCavalcante <azevedoaranha1@gmail.com>
2024-09-18 17:01:26 -0300

    feat: add events

---
+++ api/src/main/java/com/eventotec/api/domain/event/EventRequestDTO.java
@@ -0,0 +1,7 @@
+package com.eventotec.api.domain.event;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public record EventRequestDTO(String title, String description, Long date, String city, String state, Boolean remote,
+                              String eventUrl, MultipartFile image) {
+}
